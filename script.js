be13fa0e71beaefea1dfec6b2e9d8e919046c2a0
shannican <shannimura8683@gmail.com>
2024-07-06 19:27:21 +0530

    message

---
+++ script.js
@@ -0,0 +1,84 @@
+var arr = [
+  {
+    username: "sweeti kumari",
+    dp: "https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    story:
+      "https://images.unsplash.com/photo-1532767153582-b1a0e5145009?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    username: "babita kumari",
+    dp: "https://images.unsplash.com/photo-1526510747491-58f928ec870f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    story:
+      "https://images.unsplash.com/photo-1719831400714-5d4fe425cd8a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    username: "lovely kumari",
+    dp: "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fHw%3D",
+    story:
+      "https://images.unsplash.com/photo-1515372039744-b8f02a3ae446?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEzfHx8ZW58MHx8fHx8",
+  },
+  {
+    username: "sweeti kumari",
+    dp: "https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    story:
+      "https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    username: "babita kumari",
+    dp: "https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    story:
+      "https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    username: "lovely kumari",
+    dp: "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fHw%3D",
+    story:
+      "https://images.unsplash.com/photo-1515372039744-b8f02a3ae446?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEzfHx8ZW58MHx8fHx8",
+  },
+];
+
+var clutter = ""
+
+arr.forEach(function(elem,idx){
+  clutter += `<div class="story">
+            <img id=${idx} src=${elem.dp} alt="">
+          </div>`
+})
+
+var storiyan = document.querySelector("#storiyan")
+
+
+
+var full = document.querySelector("#full")
+var username = document.querySelector("#full h5")
+var growth = document.querySelector("#growth")
+var userImage = document.querySelector(".userStory img")
+
+
+storiyan.innerHTML = clutter
+
+storiyan.addEventListener("click",function(dets){
+
+  var grow = 0
+
+  var currentStory =  arr[dets.target.id].story
+
+  full.style.display = "initial"
+  full.style.backgroundImage = `url(${currentStory})`
+  username.innerHTML = arr[dets.target.id].username
+  userImage.setAttribute("src",arr[dets.target.id].dp)
+
+
+  var storyInterval = setInterval(function(){
+    grow++
+    growth.style.width = grow + "%"
+    console.log(grow)
+  },40)
+
+  setTimeout(function(){
+    full.style.display = "none"
+    clearInterval(storyInterval)
+  },4000)
+  
+
+})
